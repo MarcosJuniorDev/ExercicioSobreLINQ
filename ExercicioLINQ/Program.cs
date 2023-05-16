1c78b75d3c404203f80a431be54ca00282932de0
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-05-16 19:14:13 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioLINQ/Program.cs
@@ -0,0 +1,49 @@
+﻿using ExercicioLINQ.Entities;
+using System.Globalization;
+
+namespace ExercicioLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> list = new List<Employee>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    string email = fields[1];
+                    double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                    list.Add(new Employee(name, email, salary));
+
+                }
+            }
+
+            Console.Write("Enter the salary: ");
+            double valueSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"Email of people whose salary is more than {valueSalary.ToString("F2", CultureInfo.InvariantCulture)}:");
+
+            var emailFuncs = list.Where(p => p.Salary > valueSalary).OrderBy(p => p.Email).Select(p => p.Email);
+
+            var somaSalario = list.Where(p => p.Name[0] == 'M').Sum(p => p.Salary);
+
+            foreach (string email in emailFuncs)
+            {
+                Console.WriteLine(email);
+            }
+
+            Console.WriteLine($"Sum of salary of people whose name starts whith 'M': {somaSalario.ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+
+
+        }
+    }
+}
